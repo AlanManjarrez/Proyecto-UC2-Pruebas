fbad0689d83ab2d7300ef8fc37dd97bc8a60e4f2
AlanManjarrez <160210523+AlanManjarrez@users.noreply.github.com>
2024-10-08 01:02:21 -0700

    Presentación

---
+++ Tarea_To_Do_Presentacion/src/main/java/tarea_to_do_presentacion/main/Tarea_To_Do_Presentacion.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tarea_to_do_presentacion.main;
+
+import tarea_to_do_presentacion.frames.frm_Inicio;
+
+/**
+ *
+ * @author Jose Alan Manjarrez Ontiveros 228982
+ * @author Jesus Eduardo Villanueva Godoy 235078
+ */
+public class Tarea_To_Do_Presentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        frm_Inicio inicio = new frm_Inicio();
+        inicio.setVisible(true);
+    }
+}
